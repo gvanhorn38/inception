e80a725dede56ae6228481ac888d3c3e042b1baf
Jasdev Imani <jasdev.imani@cornell.edu>
2016-09-15 16:43:19 -0400

    Added 'bbox_stats' to check for invalid bounding boxes.

--- inputs/stat_tfrecords.py
+++ inputs/stat_tfrecords.py
@@ -77,13 +77,98 @@ def class_stats(tfrecords):
     for index in missing_values:
       print "Missing class %d" % (index,) 
     
+def bbox_stats(tfrecords):
+  """
+  Check that each example has valid bounding box data. 
+  """
+  
+  filename_queue = tf.train.string_input_producer(
+    tfrecords,
+    num_epochs=1
+  )
+
+  # Construct a Reader to read examples from the .tfrecords file
+  reader = tf.TFRecordReader()
+  _, serialized_example = reader.read(filename_queue)
+  
+  # Parse an Example to access the Features
+  features = tf.parse_single_example(
+    serialized_example,
+    features = {
+      'image/class/label' : tf.FixedLenFeature([], tf.int64),
+      'image/id' : tf.FixedLenFeature([], tf.string),
+      'image/object/bbox/xmin' : tf.VarLenFeature(dtype=tf.float32),
+      'image/object/bbox/ymin' : tf.VarLenFeature(dtype=tf.float32),
+      'image/object/bbox/xmax' : tf.VarLenFeature(dtype=tf.float32),
+      'image/object/bbox/ymax' : tf.VarLenFeature(dtype=tf.float32),
+      'image/object/bbox/label' : tf.VarLenFeature(dtype=tf.int64),
+      'image/object/id' : tf.VarLenFeature(dtype=tf.string),
+    }
+  )
+
+  #label = tf.cast(features['image/class/label'], tf.int32)
+  image_id = features['image/id']
+  
+  xmin = tf.expand_dims(features['image/object/bbox/xmin'].values, 0)
+  ymin = tf.expand_dims(features['image/object/bbox/ymin'].values, 0)
+  xmax = tf.expand_dims(features['image/object/bbox/xmax'].values, 0)
+  ymax = tf.expand_dims(features['image/object/bbox/ymax'].values, 0)
+  label = tf.expand_dims(tf.cast(features['image/object/bbox/label'].values, tf.float32), 0)
+  annot_id = tf.expand_dims(features['image/object/id'].values, 0)
+
+  image_count = 0
+  image_missing_bbox_count = 0
+  flat_bbox_count = 0
+  
+  coord = tf.train.Coordinator()
+  with tf.Session() as sess:
+
+    tf.initialize_all_variables().run()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    try:
+      
+      while not coord.should_stop():
+        
+        #outputs = sess.run([xmin, xmax, ymin, ymax, label])
+        outputs = sess.run([xmin, xmax, ymin, ymax, label, image_id, annot_id])
+        #outputs = sess.run([label])
+        
+        image_xmin = outputs[0]
+        image_xmax = outputs[1]
+        image_ymin = outputs[2]
+        image_ymax = outputs[3]
+        image_label = outputs[4]
+        id = outputs[5]
+        annotation_id = outputs[6]
+
+        image_count += 1
+        if(len(image_xmin) == 0):
+          image_missing_bbox_count += 1
+          print "MISSING bbox: image_id %s, box_id %s, Class %s, xmin %s, xmax %s, ymin %s, ymax %s" % (id, annotation_id, image_label, image_xmin, image_xmax, image_ymin, image_ymax)
+        elif image_xmin[0] >= image_xmax[0] or image_ymin[0] >= image_ymax[0]:
+          flat_bbox_count += 1
+          print "bbox ERROR: image_id %s, box_id %s, Class %s, xmin %s, xmax %s, ymin %s, ymax %s" % (id, annotation_id, image_label, image_xmin, image_xmax, image_ymin, image_ymax)
+
+        if image_count % 1000 == 0:
+          print "Processed %d images" % image_count
+
+        
+    except tf.errors.OutOfRangeError as e:
+      pass
+  
+  # Basic info
+  print "Found %d images" % (image_count,)
+  print "Found %d missing bboxes" % (image_missing_bbox_count,)
+  print "Found %d 2d bboxes" % (flat_bbox_count,)
+  
 
 def parse_args():
 
     parser = argparse.ArgumentParser(description='Basic statistics on tfrecord files')
   
     parser.add_argument('--stat', dest='stat_type',
-                        choices=['class_stats'],
+                        choices=['class_stats', 'bbox_stats'],
                         required=True)
     
     parser.add_argument('--tfrecords', dest='tfrecords',
@@ -101,4 +186,6 @@ if __name__ == '__main__':
     
     if args.stat_type == 'class_stats':
       class_stats(args.tfrecords)
-  
+    elif args.stat_type == 'bbox_stats':
+      bbox_stats(args.tfrecords)
+
